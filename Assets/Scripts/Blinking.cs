8472d3f264902e9513e71c2054153361a02a36bb
SergeRichard <serge.richard@rogers.com>
2016-08-07 18:34:38 -0300

    Added blinking script to power pellets and 1UP.

---
+++ Assets/Scripts/Blinking.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Blinking : MonoBehaviour {
+
+	public float seconds = .5f;
+
+	void Start() {
+		if (GetComponent<SpriteRenderer> () != null) {
+			StartCoroutine (BlinkSprite());
+		}
+		if (GetComponent<Text> () != null) {
+			StartCoroutine (BlinkText ());
+		}
+
+	}
+
+	IEnumerator BlinkSprite() {
+		while (true) {
+			GetComponent<SpriteRenderer> ().enabled = false;
+			yield return new WaitForSeconds (seconds);
+			GetComponent<SpriteRenderer> ().enabled = true;
+			yield return new WaitForSeconds (seconds);
+		}
+
+	}
+	IEnumerator BlinkText() {
+		while (true) {
+			GetComponent<Text> ().enabled = false;
+			yield return new WaitForSeconds (seconds);
+			GetComponent<Text> ().enabled = true;
+			yield return new WaitForSeconds (seconds);
+		}
+
+	}
+}
