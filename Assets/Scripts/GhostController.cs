e0cf61ef145846c8a09b9659ba7ebda1e571e203
SergeRichard <serge.richard@rogers.com>
2016-08-09 20:03:38 -0300

    Implementation of ghost behavior and movement (in progress).

---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostController : MonoBehaviour {
+
+	public enum Modes { Scatter, Chase };
+	public Modes Mode;
+
+	public enum Actions
+	{
+		MoveRight,
+		MoveLeft,
+		MoveDown,
+		MoveUp
+	};
+
+	// Use this for initialization
+	void Start () {
+		Mode = Modes.Scatter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
