1a7927f32aaf10134cabc2c6e58ee9f5392dfb7f
SergeRichard <serge.richard@rogers.com>
2016-10-08 10:11:12 -0300

    Title animation sequence (in progress).

--- Assets/Scripts/TitleMessageController.cs
+++ Assets/Scripts/TitleMessageController.cs
@@ -40,10 +40,58 @@ public class TitleMessageController : MonoBehaviour {
 		PowerPellet.SetActive(false);
 		TenPtsText.SetActive(false);
 		FiftyPtsText.SetActive(false);
+
+		StartCoroutine (StartAnimation ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	IEnumerator StartAnimation() {
+		// Row 1
+		yield return new WaitForSeconds (.5f);
+		RedGhost.SetActive (true);
+
+		yield return new WaitForSeconds (1f);
+		ShadowText.SetActive (true);
+
+		yield return new WaitForSeconds (.5f);
+		BlinkyText.SetActive (true);
+
+		// row 2
+		yield return new WaitForSeconds (.5f);
+		PinkGhost.SetActive (true);
+
+		yield return new WaitForSeconds (1f);
+		SpeedyText.SetActive (true);
+
+		yield return new WaitForSeconds (.5f);
+		PinkyText.SetActive (true);
+
+		// row 3
+		yield return new WaitForSeconds (.5f);
+		BlueGhost.SetActive (true);
+
+		yield return new WaitForSeconds (1f);
+		BashfulText.SetActive (true);
+
+		yield return new WaitForSeconds (.5f);
+		InkyText.SetActive (true);
+
+		// row 4
+		yield return new WaitForSeconds (.5f);
+		YellowGhost.SetActive (true);
+
+		yield return new WaitForSeconds (1f);
+		PokeyText.SetActive (true);
+
+		yield return new WaitForSeconds (.5f);
+		ClydeText.SetActive (true);
+
+
+
+	}
+
 }
